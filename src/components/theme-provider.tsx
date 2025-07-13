901739a520fc41ade6fc812ce93b74fc9632f797
vasilhstaf <tamoutselhsbasilhs@gmail.com>
2025-07-13 04:06:34 +0300

    Force light mode for all users

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -26,9 +26,7 @@ export function ThemeProvider({
   storageKey = "vite-ui-theme",
   ...props
 }: ThemeProviderProps) {
-  const [theme, setTheme] = useState<Theme>(
-    () => (localStorage.getItem(storageKey) as Theme) || defaultTheme
-  )
+  const [theme, setTheme] = useState<Theme>("light");
 
   useEffect(() => {
     const root = window.document.documentElement
